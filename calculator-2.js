22dca2a1509536bf60c7ab1f37c19ec739f04b55
tbonemalone <tony.org@gmail.com>
2011-02-19 07:38:46 -0800

    added poor error checking to test it out.

--- calculator-2.js
+++ calculator-2.js
@@ -1,29 +1,41 @@
 
 function aTest(){
-	var testForm = document.forms["calculator"];
+	var calcForm = document.forms["calculator"];
 	
 	// calc income
-	var income = testForm["income"].value;
-	income = Number(income);
+	// fist go at validating fields.
+	// checking to see that something entered into income field
+	// this works for this field but is a terrible implementation. 
+	// need to give inline error messages
+	if (calcForm.income.value == "" || calcForm.income.value == null)
+		{
+			document.write("Please enter income");
+		}
+	else
+		{
+			var income = calcForm["income"].value;
+			income = Number(income);
+		}
+
 	var taxes = income * .28;
-	testForm["taxes"].value = taxes.toFixed(2);
+	calcForm["taxes"].value = taxes.toFixed(2);
 	netIncome = income - taxes;
 	netIncome = String(netIncome.toFixed(0));
-	testForm["netIncome"].value = "$" + netIncome;
+	calcForm["netIncome"].value = "$" + netIncome;
 	
 	// calculating total expenses
-	var housing = testForm["housing"].value;
+	var housing = calcForm["housing"].value;
 	housing = Number(housing);
 	
-	var utilities = testForm["utilities"].value;
+	var utilities = calcForm["utilities"].value;
 	utilities = Number(utilities);
 	
-	var studentLoans = testForm["studentLoans"].value;
+	var studentLoans = calcForm["studentLoans"].value;
 	studentLoans = Number(studentLoans);
 
 	var totalMonthlyExpenses = housing + utilities + studentLoans; 
 	totalMonthlyExpenses = String(totalMonthlyExpenses.toFixed(0));
-	testForm["totalMonthlyExpenses"].value = "$" + totalMonthlyExpenses;
+	calcForm["totalMonthlyExpenses"].value = "$" + totalMonthlyExpenses;
 
 }
 
